4f549e43aa2d88f1f72b9f1f3b7ad25271a6234f
Ayush Kulshrestha <akcool.ayush@gmail.com>
2021-06-06 14:13:18 +0530

    final youtube video commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { css, ThemeProvider } from "styled-components";
+
+const theme = {
+  primary: "blue",
+  secondary: "cyan",
+  font: "sans-serif",
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px;
+  & input {
+    width: auto;
+  }
+  @media (max-width: 720px) {
+    & input {
+      width: 100%;
+    }
+  }
+`;
+
+const HeadingComponent = styled.h1`
+  font-family: ${(props) => props.theme.font};
+`;
+
+const Input = styled.input`
+  padding: 5px;
+  border-radius: 4px;
+  margin: 10px;
+`;
+
+const Button = styled.button`
+  ${(props) =>
+    props.isValid
+      ? css`
+          color: white;
+          background: green;
+        `
+      : css`
+          color: red;
+          background: yellow;
+        `}
+  padding: 5px;
+
+  &:hover {
+    background: ${(props) => props.theme.secondary};
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <HeadingComponent>Styled Components</HeadingComponent>
+        <Input placeholder="username" />
+        <Input placeholder="password" />
+        <Button isValid={true}>Login</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
